36a9ba5be3675c18f0fd42e4a6b4157e61deda30
Bruno Augusto Lopes Fevereiro <br.fevereiro@terra.com.br>
2021-05-10 16:07:01 -0300

    adding the form part

--- app.js
+++ app.js
@@ -1,17 +1,48 @@
+const generateButton = document.getElementById('generateButton')
+const addButton = document.getElementById('addButton')
+
 const textSpace = document.getElementById('randomText')
+const newForm = document.getElementsByClassName('hidden')
 
-generateRandomPhase = () => {
-    const randomNum = Math.floor(Math.random()*3) 
+const phase = document.querySelector('#newPhase').value
+const language = document.querySelector('#language').value
+//generateButton.addEventListener('click', this.callFunc)
 
-    if (randomNum === 0) {
-        return 'Add yours phases for they apear!!'
-    } else if (randomNum === 1) {
-        return 'Hi'
-    } else if (randomNum === 2 ) {
-        return 'Porra'
+const showForm = () => {
+    for (let i = 0; i < newForm.length; i++) {
+        if (addButton.click) {
+            if (newForm[i].style.display === 'none') {
+                newForm[i].style.display = 'block'
+            } else {
+                newForm[i].style.display = 'none'
+            }
+        }
     }
 }
 
-callFunc = () => {
-    textSpace.innerHTML = generateRandomPhase()
+const phases = {
+    _Phases: {},
+
+    createPhase() {
+        const newP = [`${phase}, ${language}`]
+        return _Phases.push(newP)
+    },
+
+    generateRandomPhase() {
+        const randomNum = Math.floor(Math.random()*3) 
+    
+        if (randomNum === 0) {
+            return 'Add yours phases for they apear!!'
+        } else if (randomNum === 1) {
+            return 'Hi'
+        } else if (randomNum === 2 ) {
+            return 'Porra'
+        }
+    },
+    
+    callFunc () {
+        textSpace.innerHTML = this.generateRandomPhase()
+    }
 }
+const test = document.getElementById('test')
+test.innerHTML = phase
